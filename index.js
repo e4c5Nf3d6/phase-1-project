e97a74f9891ddd862ed7e2f3c591008d10a945cb
Maria Jenkins <macjenkin@gmail.com>
2023-05-08 13:19:16 -0500

    Make search display basic user stats

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
 // Definitions
 let form = document.querySelector("#user-search")
-let userInfo = document.querySelector("#user-info")
+let userGames = document.querySelector("#user-games")
+let userStats = document.querySelector("#user-stats")
 
 // Functions
 function searchUser(e) {
     e.preventDefault()
 
-    // displayUserInfo(e)
+    displayUserStats(e)
     displayUserGames(e)
 
     form.reset()
 }
 
-// function displayUserInfo(e) {
+function displayUserStats(e) {
+    let username = e.target.querySelector('#username').value
+    
+    fetch(`https://lichess.org/api/user/${username}`, {
+        method: 'GET',
+        headers: {
+            'Accept': "application/x-ndjson"
+        }
+    })
+    .then(res => res.text())
+    .then(body => {
+        let str = "[" + body.replace(/\r?\n/g, ",").replace(/,\s*$/, "") + "]"
+        return JSON.parse(str)
+    })
+    .then(data => {
+        console.log(data)
+        userStats.innerHTML = ''
+        
+        let name = document.createElement('h2')
+        name.textContent = data[0].username
 
-// }
+        let blitzRating = document.createElement('p')
+        blitzRating.textContent = `Blitz Rating: ${data[0].perfs.blitz.rating}`
+
+        userStats.append(name, blitzRating)
+    })
+}
 
 function displayUserGames(e) {
     let username = e.target.querySelector('#username').value
@@ -30,10 +55,11 @@ function displayUserGames(e) {
         return JSON.parse(str)
     })
     .then(games => {
+        userGames.innerHTML = ''
         games.forEach(game => {
             let gameTitle = document.createElement('h4')
             gameTitle.textContent = `${game.players.white.user.name} vs. ${game.players.black.user.name}`
-            userInfo.appendChild(gameTitle)
+            userGames.appendChild(gameTitle)
         })
     })
 }
