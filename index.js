448c0de75c4b0b847fccee254da9c6f0d84531bf
Maria Jenkins <macjenkin@gmail.com>
2023-05-12 17:53:53 -0500

    fix displayUserStats response type

--- index.js
+++ index.js
@@ -38,38 +38,77 @@ function searchOpenings(e) {
     openingForm.reset()
 }
 
+// function displayUserStats(e) {
+//     let username = e.target.querySelector('#username').value
+    
+//     fetch(`https://lichess.org/api/user/${username}`, {
+//         method: 'GET',
+//         headers: {
+//             'Accept': "application/x-ndjson"
+//         }
+//     })
+//     .then(res => res.text())
+//     .then(body => {
+//         let str = "[" + body.replace(/\r?\n/g, ",").replace(/,\s*$/, "") + "]"
+//         return JSON.parse(str)
+//     })
+//     .then(data => {
+//         let name = document.createElement('h2')
+//         let link = document.createElement('a')
+//         link.id = 'username-display'
+//         link.href = data[0].url
+//         link.textContent = data[0].username
+//         link.target = '_blank'
+
+//         name.append(link)
+
+//         let blitzRating = document.createElement('p')
+//         blitzRating.textContent = `Blitz Rating: ${data[0].perfs.blitz.rating}`
+
+//         let rapidRating = document.createElement('p')
+//         rapidRating.textContent = `Rapid Rating: ${data[0].perfs.rapid.rating}`
+
+//         let classicalRating = document.createElement('p')
+//         classicalRating.textContent = `Classical Rating: ${data[0].perfs.classical.rating}`
+
+//         userStats.append(name, blitzRating, rapidRating, classicalRating)
+
+//         toggle.className = 'visible'
+//         userInfo.className = 'visible'        
+//     })
+//     .catch(error => {
+//         errorBox.textContent = `It looks like ${username} doesn't like to play chess.`
+//     })
+// }
+
 function displayUserStats(e) {
     let username = e.target.querySelector('#username').value
     
     fetch(`https://lichess.org/api/user/${username}`, {
         method: 'GET',
         headers: {
-            'Accept': "application/x-ndjson"
+            'Accept': "application/json"
         }
     })
-    .then(res => res.text())
-    .then(body => {
-        let str = "[" + body.replace(/\r?\n/g, ",").replace(/,\s*$/, "") + "]"
-        return JSON.parse(str)
-    })
-    .then(data => {
+    .then(res => res.json())
+    .then(user => {
         let name = document.createElement('h2')
         let link = document.createElement('a')
         link.id = 'username-display'
-        link.href = data[0].url
-        link.textContent = data[0].username
+        link.href = user.url
+        link.textContent = user.username
         link.target = '_blank'
 
         name.append(link)
 
         let blitzRating = document.createElement('p')
-        blitzRating.textContent = `Blitz Rating: ${data[0].perfs.blitz.rating}`
+        blitzRating.textContent = `Blitz Rating: ${user.perfs.blitz.rating}`
 
         let rapidRating = document.createElement('p')
-        rapidRating.textContent = `Rapid Rating: ${data[0].perfs.rapid.rating}`
+        rapidRating.textContent = `Rapid Rating: ${user.perfs.rapid.rating}`
 
         let classicalRating = document.createElement('p')
-        classicalRating.textContent = `Classical Rating: ${data[0].perfs.classical.rating}`
+        classicalRating.textContent = `Classical Rating: ${user.perfs.classical.rating}`
 
         userStats.append(name, blitzRating, rapidRating, classicalRating)
 
